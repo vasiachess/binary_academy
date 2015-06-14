97a33e57725181af1b8d92dd7ec07a4635df854a
Vasyl <vasiachess@yandex.ua>
2015-06-14 19:07:56 +0300

    jquery

--- hometask_4/scripts/main.js
+++ hometask_4/scripts/main.js
@@ -3,18 +3,48 @@ var Application = {};
 (function (Application){
     var
         _document,
-        _container;
+        _$container;
         id = 0;
 
     Application.init = function(document) {
         _document = document;
-        _container = _document.getElementById('todo-container');
+        _$container = $('#base-container');
 
-        this.addTaskContainer(_container);
+        this.addTaskContainer(_$container);
+        _$container.click($.proxy(this.containerOnClick, this));
     };
 
-    Application.addTaskContainer = function(baseNode) {
-        var containerElement = _document.createElement
+    Application.addTaskContainer = function($baseNode) {
+
+        var id = this.nextId();
+
+        $baseNode.append($('<ul id="' + id + '"></ul>'));
+        $baseNode.append($('<input type="text" data-task-container-id="' + id + '"></input>'));
+        $baseNode.append($('<button data-action="AddGood" data-task-container-id="' + id + '">Add</button>'));
+
+        $("#txt").click(function(){
+            var $ctrl = $('<input/>').attr({ type: 'checkbox', name:'chk'}).addClass("chk");
+
+            $("#goods-container").append($ctrl);
+            $("#goods-container").append(txtPlain.value);
+        });
     };
+
+    Application.containerOnClick = function(evt) {
+        if (evt.target.hasAttribute('data-action')) {
+            switch (evt.target.getAttribute('data-action')) {
+                case 'addGood': $("#goods-container").append("one");
+            }
+        }
+    };
+
+    Application.proxy = function(hander, owner) {
+        return handler.bind(owner);
+    };
+
+    Application.nextId = function() {
+        return id++;
+    };
+
 
 })(Application);
