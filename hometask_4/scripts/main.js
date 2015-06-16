63b5161406ef7baf7034ddf7c32ed9f5470c8d88
Vasyl Prokopyshyn <vasiachess@gmail.com>
2015-06-16 18:02:29 +0300

    Add goods

--- hometask_4/scripts/main.js
+++ hometask_4/scripts/main.js
@@ -8,34 +8,52 @@ var Application = {};
 
     Application.init = function(document) {
         _document = document;
-        _$container = $('#base-container');
+        _$container = $('#goods-container');
 
-        this.addTaskContainer(_$container);
+        this.addGoodsContainer(_$container);
+
         _$container.click($.proxy(this.containerOnClick, this));
     };
 
-    Application.addTaskContainer = function($baseNode) {
+    Application.addGoodsContainer = function($baseNode) {
 
         var id = this.nextId();
 
         $baseNode.append($('<ul id="' + id + '"></ul>'));
-        $baseNode.append($('<input type="text" data-task-container-id="' + id + '"></input>'));
-        $baseNode.append($('<button data-action="AddGood" data-task-container-id="' + id + '">Add</button>'));
+        $baseNode.append($('<input type="text" id="inputTxt" data-task-container-id="' + id + '"></input>'));
+        $baseNode.append($('<button data-action="AddGoods" data-task-container-id="' + id + '">Add</button>'));
 
-        $("#txt").click(function(){
-            var $ctrl = $('<input/>').attr({ type: 'checkbox', name:'chk'}).addClass("chk");
-
-            $("#goods-container").append($ctrl);
-            $("#goods-container").append(txtPlain.value);
-        });
     };
 
     Application.containerOnClick = function(evt) {
+
         if (evt.target.hasAttribute('data-action')) {
+
             switch (evt.target.getAttribute('data-action')) {
-                case 'addGood': $("#goods-container").append("one");
+                case "AddGoods": this.onAddGoods(_$container); break;
+                case "DeleteGoods": this.onDeleteGoods(evt); break;
+
             }
         }
+    };
+
+    Application.onAddGoods = function($baseNode) {
+
+        var id = this.nextId();
+
+        console.log("addGoods id: " + id);
+
+        $baseNode.append($('<ul id="' + id + '"></ul>'));
+        $baseNode.append($('<input type="checkbox" data-goods-id="' + id + '"></input>'));
+        $baseNode.append(inputTxt.value);
+        $baseNode.append($('<button data-action="DeleteGoods" data-goods-id="' + id + '">x</button>'));
+
+    };
+
+    Application.onDeleteGoods = function(evt) {
+        var goodsContainer = _document.getElementById(evt.target.getAttribute('data-goods-id'));
+
+        goodsContainer.parentNode.removeChild(goodsContainer);
     };
 
     Application.proxy = function(hander, owner) {
@@ -47,4 +65,4 @@ var Application = {};
     };
 
 
-})(Application);
+})(Application, jQuery);
