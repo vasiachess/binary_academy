fea0b1cc1777a3ca3a8a3b8da18cc1422d70f342
Vasyl <vasiachess@yandex.ua>
2015-06-16 23:35:09 +0300

    Clear

--- hometask_4/scripts/main.js
+++ hometask_4/scripts/main.js
@@ -13,6 +13,7 @@ var Application = {};
         this.addGoodsContainer(_$container);
 
         _$container.click($.proxy(this.containerOnClick, this));
+        $('#clear-container').click($.proxy(this.containerOnClickClear, this));
     };
 
     Application.addGoodsContainer = function($baseNode) {
@@ -23,6 +24,8 @@ var Application = {};
         $baseNode.append($('<input type="text" id="inputTxt" data-task-container-id="' + id + '"></input>'));
         $baseNode.append($('<button data-action="AddGoods" data-task-container-id="' + id + '">Add</button>'));
 
+
+
     };
 
     Application.containerOnClick = function(evt) {
@@ -32,7 +35,18 @@ var Application = {};
             switch (evt.target.getAttribute('data-action')) {
                 case "AddGoods": this.onAddGoods(_$container); break;
                 case "DeleteGoods": this.onDeleteGoods(evt); break;
+                //case "Check": this.onCrossOutGoods(evt); break;
 
+            }
+        }
+    };
+
+    Application.containerOnClickClear = function(evt) {
+
+        if (evt.target.hasAttribute('data-action')) {
+
+            switch (evt.target.getAttribute('data-action')) {
+                case "ClearGoods": this.onClearGoods(); break;
             }
         }
     };
@@ -41,11 +55,12 @@ var Application = {};
 
         var id = this.nextId();
             $newGoods = $('<ul id="' + id + '"></ul>'),
-            $inputNew = $('<input data-task-id:"' + id + '"/>').attr({ type: 'text', name:'text', value:inputTxt.value, id:'currentInput'}).prop('disabled', true);
+            $inputNew = $('<input type="text" data-goods-id:"' + id + '"/>').attr({ value:inputTxt.value, id:'currentInput', readonly:'readonly'});
+            $inputCheckBox =$('<input type="checkbox" data-action="Check" data-goods-id="' + id + '"></input>');
 
         console.log("addGoods id: " + id);
 
-        $newGoods.append('<input type="checkbox" data-goods-id="' + id + '"></input>');
+        $newGoods.append($inputCheckBox);
         $newGoods.append($inputNew);
         $newGoods.append('<button data-action="DeleteGoods" data-goods-id="' + id + '">x</button>');
 
@@ -57,6 +72,32 @@ var Application = {};
         $('#' + $(evt.target).attr('data-goods-id')).remove();
     };
 
+    Application.onClearGoods = function() {
+        console.log("Clear");
+
+
+    };
+
+
+    $(function () {
+        $(_document).on('change', 'input:checkbox', function () {
+            var input = $(this).next('input');
+            if (this.checked) {
+                $(input).css('textDecoration', 'line-through');
+            } else {
+                $(input).css('textDecoration', 'none');
+            }
+        })
+    });
+
+    $(function () {
+        $(_document).on('dblclick', 'input:text', function () {
+            var input = $(this);
+                $(input).removeAttr("readonly");
+            console.log("double click");
+        })
+    });
+
     Application.proxy = function(hander, owner) {
         return handler.bind(owner);
     };
@@ -65,5 +106,5 @@ var Application = {};
         return id++;
     };
 
-
 })(Application, jQuery);
+
