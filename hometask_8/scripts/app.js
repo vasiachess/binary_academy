da64f12b34b25c6c349ddc1107869f9d9dcc3974
Vasyl <vasiachess@yandex.ua>
2015-07-14 23:26:31 +0300

    Customers

--- hometask_8/scripts/app.js
+++ hometask_8/scripts/app.js
@@ -1,10 +1,19 @@
 angular.module('app', [])
-    .controller('firstController', function($scope){
+    .controller('goodsController', function($scope){
         $scope.goods = [{name: 'apple', price: 5}, {name: 'tomato', price: 4}];
    
         $scope.showGoods = true;
 
         $scope.toggleView = function(){
-           $scope.showGoods = !$scope.showGoods; 
+           $scope.showGoods = !$scope.showGoods;
+        };
+    })
+    .controller('customersController', function($scope){
+        $scope.customers = [{name: 'Sam', city: 'London', age: 25, avatar:''}, {name: 'John', city: 'Manchester', age: 31, avatar:''}];
+
+        $scope.showCustomers = true;
+
+        $scope.toggleView2 = function(){
+            $scope.showCustomers = !$scope.showCustomers;
         };
     })
